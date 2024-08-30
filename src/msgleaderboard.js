fd077cccc509616d3f8d5e9ff6bb568f853789a4
Grezaski <123626705+Grezaski@users.noreply.github.com>
2024-08-30 11:35:38 +0530

    Revert "server-status and mc server status commands "

--- src/msgleaderboard.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-let msgSchema = new Schema({
-    Guild: String,
-    User: String,
-    Messages: Number
-});
-
-module.exports = model('msgSchema198371223342342349', msgSchema);
